1138fb9521dca7d7152ff8015dbea1833a7d0a85
viragorkenyi <viragorkenyi@gmail.com>
2019-04-07 23:10:14 +0200

    fe

---
+++ projekt/fe/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  <h1>Rendelését megkaptuk!</h1>
+  </div>
+</template>
+
+
+
+<style>
+#app {
+  font-family: "Times New Roman", Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  background-color: #8a8a5c;
+  border-radius: 8px;
+  font-size:20px;
+  padding:200px
+  }
+
+</style>
